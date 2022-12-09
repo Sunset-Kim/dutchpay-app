91a631db43d733ce5ed80b490be9ba01670c5ed2
Sunset-kim <wood2002@naver.com>
2022-12-09 18:39:42 +0900

    setup: use mantine ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Grid from "../components/layouts/Grid";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <ChakraProvider>
+          <Grid>
+            <Component {...pageProps} />
+          </Grid>
+        </ChakraProvider>
+      </MantineProvider>
+    </>
+  );
 }
