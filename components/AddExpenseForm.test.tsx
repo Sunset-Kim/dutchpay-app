9702dc0d0e70580623fc7700052b6aaa9dd9105d
Sunset-kim <wood2002@naver.com>
2022-12-09 18:39:08 +0900

    red: fix test AddExpenseForm (정산정보입력)

--- components/AddExpenseForm.test.tsx
+++ components/AddExpenseForm.test.tsx
@@ -7,14 +7,16 @@ const renderComponent = (members: string[]) => {
   render(<AddExpenseForm onSubmit={handleSubmit} members={members} />);
 
   const options = screen.queryAllByRole("option");
-  const price = screen.getByRole("spinbutton", { name: "price" });
-  const desc = screen.getByRole("textbox", { name: "description" });
+  const price = screen.getByRole("textbox", { name: "Price" });
+  const desc = screen.getByRole("textbox", { name: "Description" });
+  const addBtn = screen.getByRole("button");
   const calendarToggle = screen.getByRole("checkbox");
 
   return {
     options,
     price,
     desc,
+    addBtn,
     calendarToggle,
     handleSubmit,
   };
@@ -25,40 +27,45 @@ describe("AddExpenseForm(정산정보 입력폼)", () => {
     const { options, price, desc } = renderComponent(["김영식", "김민우"]);
     expect(options).toHaveLength(3);
     expect(
-      screen.getByRole("option", { name: "비용을 지불한 사람을 선택해주세요", selected: true }) as HTMLOptionElement
+      screen.getByRole("option", { name: "결제한 사람을 선택해주세요", selected: true }) as HTMLOptionElement
     ).not.toBeNull();
     expect(price).toBeInTheDocument();
     expect(desc).toBeInTheDocument();
   });
 
-  it("캘린더 보기를 선택하면, 캘린더가 렌더링 된다", async () => {
-    const { calendarToggle } = renderComponent(["김영식", "김민우"]);
-    await userEvent.click(calendarToggle);
-    expect(screen.getByTestId("calendar")).toBeInTheDocument();
-  });
+  //TODO: Calendar Component 완성이후
+  // it.todo("캘린더 보기를 선택하면, 캘린더가 렌더링 된다", async () => {
+  //   const { calendarToggle } = renderComponent(["김영식", "김민우"]);
+  //   await userEvent.click(calendarToggle);
+  //   expect(screen.getByTestId("calendar")).toBeInTheDocument();
+  // });
 
   it("input을 모두 입력하지 않으면 에러메세지가 나온다", async () => {
     const { options, price, desc } = renderComponent(["김영식", "김민우"]);
-    expect(screen.queryAllByRole("alertdialog")).toHaveLength(3);
+    expect(screen.queryAllByRole("alert")).toHaveLength(2);
 
     await userEvent.selectOptions(screen.getByRole("combobox"), screen.getByRole("option", { name: "김영식" }));
-    expect(screen.getByRole("combobox").getAttribute("value")).not.toBeNull();
-    expect(screen.queryAllByRole("alertdialog")).toHaveLength(2);
+    expect(screen.getByRole("option", { name: "김영식" })).toBeInTheDocument();
+    expect(screen.queryAllByRole("alert")).toHaveLength(1);
 
     await userEvent.type(price, "20000");
-    expect(screen.queryAllByRole("alertdialog")).toHaveLength(1);
+    expect(screen.queryAllByRole("alert")).toHaveLength(0);
 
     await userEvent.type(desc, "차비");
-    expect(screen.queryAllByRole("alertdialog")).toHaveLength(0);
+    expect(screen.queryAllByRole("alert")).toHaveLength(0);
   });
 
   it("모든 값이 입력되고 저장을 누르면 onSubmit이 실행된다", async () => {
-    const { options, price, desc, handleSubmit } = renderComponent(["김영식", "김민우"]);
+    const { options, price, desc, handleSubmit, addBtn } = renderComponent(["김영식", "김민우"]);
 
-    await userEvent.selectOptions(screen.getByRole("combobox"), options[0]);
+    await userEvent.selectOptions(screen.getByRole("combobox"), options[1]);
+    expect(options[0]).toBeInTheDocument();
     await userEvent.type(price, "20000");
     await userEvent.type(desc, "차비");
 
+    await userEvent.click(addBtn);
+
+    expect(handleSubmit).toBeCalled();
     expect(handleSubmit).toBeCalledWith({
       payer: "김영식",
       price: 20000,
