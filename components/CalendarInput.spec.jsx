9a58c8dbb47393b8a645f656a14075e00974a40e
Sunset-kim <wood2002@naver.com>
2022-12-13 11:46:42 +0900

    red: calendar input test

---
+++ components/CalendarInput.spec.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import CalendeInput from "./CalendarInput";
+import userEvent from "@testing-library/user-event";
+
+const date = new Date();
+const handleChange = jest.fn();
+const renderComponent = ({ onChange }) => {
+  render(<CalendeInput onChange={onChange} value={date} />);
+  return {
+    handleChange,
+  };
+};
+
+beforeEach(() => {
+  renderComponent({ onChange: handleChange });
+});
+describe("calender input", () => {
+  it("render", () => {
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+    expect(input.value).toEqual(
+      `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? "0" + date.getMonth() + 1 : date.getMonth() + 1}-${
+        date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
+      }`
+    );
+  });
+
+  it("when input click, calendar display", async () => {
+    const input = screen.getByRole("textbox");
+
+    await userEvent.click(input);
+
+    const calendar = screen.getByTestId("calendar");
+    expect(calendar).toBeInTheDocument();
+  });
+
+  it("when date button click, called onChange function", async () => {
+    const input = screen.getByRole("textbox");
+    await userEvent.click(input);
+    await userEvent.click(screen.getByText("15"));
+    expect(handleChange).toBeCalled();
+  });
+});
