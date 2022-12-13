418cc1bf2a2e1d57de83cc0bc9c1ca9bd2a407c3
Sunset-kim <wood2002@naver.com>
2022-12-13 16:52:33 +0900

    ui: expense list

--- components/ExpenseList.tsx
+++ components/ExpenseList.tsx
@@ -1,6 +1,8 @@
+import { ActionIcon, Group, ScrollArea, Table, Text } from "@mantine/core";
 import { ExpenseInfo } from "../types/Expense.type";
-import ExpenseItem from "./ExpenseItem";
+import dayjs from "dayjs";
 import NoContent from "./NoContent";
+import { IconPencil, IconTrash } from "@tabler/icons";
 
 interface ExpenseListProps {
   list: ExpenseInfo[];
@@ -11,13 +13,48 @@ export default function ExpenseList({ list }: ExpenseListProps) {
     return <NoContent>정산정보를 추가해 주세요</NoContent>;
   }
 
+  const rows = list.map((expense, i) => {
+    const { date, desc, payer, price } = expense;
+    return (
+      <tr key={`expense${i}`}>
+        <td>
+          <Text>{date ? dayjs(date).format("YYYY-MM-DD") : "-"}</Text>
+        </td>
+        <td>
+          <Text size={"sm"}>{payer}</Text>
+        </td>
+        <td>{price}</td>
+        <td>
+          <Text align="left">{desc ?? "-"}</Text>
+        </td>
+        <td>
+          <Group spacing={0} position="right">
+            <ActionIcon>
+              <IconPencil size={16} stroke={1.5} />
+            </ActionIcon>
+            <ActionIcon color="red">
+              <IconTrash size={16} stroke={1.5} />
+            </ActionIcon>
+          </Group>
+        </td>
+      </tr>
+    );
+  });
+
   return (
-    <ul>
-      {list.map((expense, i) => (
-        <li key={`expense${i}`}>
-          <ExpenseItem data={expense} />
-        </li>
-      ))}
-    </ul>
+    <ScrollArea>
+      <Table verticalSpacing="sm" align="center">
+        <thead>
+          <tr>
+            <th>날짜</th>
+            <th>사용자</th>
+            <th>비용</th>
+            <th>메모</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </ScrollArea>
   );
 }
--- fixture/expense/index.ts
+++ fixture/expense/index.ts
@@ -15,17 +15,21 @@ export const EXPENSE_INFO_LIST: ExpenseInfo[] = [
   {
     payer: "김영식",
     price: 100000,
+    date: new Date("2022-12-12"),
   },
   {
     payer: "김철수",
     price: 20000,
+    date: new Date("2022-12-12"),
   },
   {
     payer: "오박사",
     price: 90000,
+    date: new Date("2022-12-12"),
   },
   {
     payer: "김영식",
     price: 9000,
+    date: new Date("2022-12-12"),
   },
 ];
