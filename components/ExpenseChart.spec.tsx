e28b050513e3210dd67d20824a84f070b7768e64
Sunset-kim <wood2002@naver.com>
2022-12-15 22:26:37 +0900

    red: chart ui 필수 구현사항 test

---
+++ components/ExpenseChart.spec.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { EXPENSE_INFO_LIST } from "../fixture/expense";
+import ExpenseChart from "./ExpenseChart";
+
+const default_data = [
+  {
+    payer: "a",
+    price: 10000,
+    part: 50,
+  },
+  {
+    payer: "b",
+    price: 10000,
+    part: 50,
+  },
+];
+
+const default_total = 20000;
+
+const renderSummary = () => {
+  render(<ExpenseChart data={default_data} total={default_total} />);
+};
+
+describe("ExpenseSummary", () => {
+  it("최종정산금액이 render 된다", () => {
+    renderSummary();
+
+    expect(screen.getByText(/최종정산금액/)).toBeInTheDocument();
+    expect(screen.getByText(/20,000/)).toBeInTheDocument();
+  });
+});
