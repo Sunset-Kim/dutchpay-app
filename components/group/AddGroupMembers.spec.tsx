d29e5bb94ce56ded1b7e681c8f9587a58135cfde
Sunset-kim <wood2002@naver.com>
2022-12-22 02:07:37 +0900

    test: 멤버등록창에서 멤버삭제 테스트

--- components/group/AddGroupMembers.spec.tsx
+++ components/group/AddGroupMembers.spec.tsx
@@ -3,12 +3,14 @@ import userEvent from "@testing-library/user-event";
 import AddMembers from "./AddGroupMembers";
 
 const handleSubmit = jest.fn();
+const onDelete = jest.fn();
 const EMPTY_MEMBERS: string[] = [];
+const MEMBERS = ["a", "b"];
 
 const renderComponent = (members: string[]) => {
-  render(<AddMembers members={members} onSubmit={handleSubmit} />);
+  render(<AddMembers members={members} onSubmit={handleSubmit} onDelete={onDelete} />);
   const input = screen.getByRole("textbox");
-  const button = screen.getByRole("button");
+  const button = screen.getByRole("button", { name: /추가/ });
 
   return {
     input,
@@ -17,7 +19,7 @@ const renderComponent = (members: string[]) => {
 };
 describe("AddMembers", () => {
   it("렌더링시 input과 button이 렌더링 된다", () => {
-    const { input, button } = renderComponent(EMPTY_MEMBERS);
+    const { input, button } = renderComponent(MEMBERS);
 
     expect(input).toBeInTheDocument();
     expect(button).toBeInTheDocument();
@@ -33,9 +35,24 @@ describe("AddMembers", () => {
 
   it("입력값이 있다면, 입력값이 전달된다", async () => {
     const { button, input } = renderComponent(EMPTY_MEMBERS);
-
     await userEvent.type(input, "유저1");
     await userEvent.click(button);
     expect(handleSubmit).toBeCalledWith("유저1");
+  });
+
+  it("멤버가 있다면, 멤버를 렌더링 한다", () => {
+    renderComponent(MEMBERS);
+
+    expect(screen.getByText(MEMBERS[0])).toBeInTheDocument();
+    expect(screen.getByText(MEMBERS[1])).toBeInTheDocument();
+  });
+
+  it("삭제 버튼을 누르면 해당 멤버가 삭제된다", async () => {
+    renderComponent(MEMBERS);
+
+    const deleteBtn = screen.getAllByRole("button", { description: /delete/ });
+
+    await userEvent.click(deleteBtn[0]);
+    expect(onDelete).toBeCalled();
   });
 });
