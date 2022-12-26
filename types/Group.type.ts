3b4cca267ca802e951ebde7dcffbe7397962d7f2
Sunset-kim <wood2002@naver.com>
2022-12-27 00:55:28 +0900

    feat: 정산리스트 localstorage 연동

--- types/Group.type.ts
+++ types/Group.type.ts
@@ -1,7 +1,9 @@
+import { ExpenseInfo } from "./Expense.type";
 import { Member } from "./Member.type";
 
 export interface IGroup {
   id: string;
   name: string;
   members: Member[];
+  expenseList?: ExpenseInfo[];
 }
