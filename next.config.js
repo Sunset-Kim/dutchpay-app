3caa832347836f696dbd3cae03aef062d76c4e67
Sunset-kim <wood2002@naver.com>
2023-01-02 18:41:40 +0900

    feat: zod,axios 패키지 설치

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+
+function getDomainHost() {
+  if (process.env.HOST && process.env.PROTOCOL && process.env.PORT) {
+    if (process.env.PROTOCOL === "http" && process.env.PORT === "80") {
+      return `${process.env.PROTOCOL}://${process.env.HOST}`;
+    }
+    if (process.env.PROTOCOL === "https" && process.env.PORT === "443") {
+      return `${process.env.PROTOCOL}://${process.env.HOST}`;
+    }
+    return `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`;
+  }
+  return "http://localhost:3000";
 }
 
-module.exports = nextConfig
+const DOMAIN_HOST = getDomainHost();
+
+const nextConfig = {
+  env: {
+    DOMAIN_HOST,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  publicRuntimeConfig: {
+    publicApiKey: process.env.publicApiKey || "",
+    authDomain: process.env.FIREBASE_AUTH_HOST || "",
+    projectId: process.env.projectId || "",
+  },
+};
+
+module.exports = nextConfig;
